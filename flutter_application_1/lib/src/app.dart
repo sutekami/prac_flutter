05153d5b77525f0f1e8796ea6719b7160ca3adb4
sutekami <gigamasi@gmail.com>
2023-09-03 14:29:07 +0900

    fisrt commit

---
+++ flutter_application_1/lib/src/app.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home.dart';
+import 'screens/favorite.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  static const _screens = [
+    HomeScreen(),
+    FavoriteScreen(),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'お気に入り')
+          ],
+          type: BottomNavigationBarType.fixed),
+    );
+  }
+}
---
+++ flutter_application_1/lib/src/screens/favorite.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('お気に入り'),
+      ),
+      body: const Center(
+        child: Text(
+          'お気に入り',
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+    );
+  }
+}
