f20910d5c9ecede2ab7529896d0a6a5b8fe894cd
sutekami <gigamasi@gmail.com>
2023-09-04 23:05:02 +0900

    httpを使ってQiitaAPIを叩く処理を実行

--- flutter_application_1/lib/src/screens/home.dart
+++ flutter_application_1/lib/src/screens/home.dart
@@ -1,7 +1,38 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> fetchData() async {
+    String url = "https://qiita.com/api/v2/items";
+    String token = "859a79cf51d8018db9e9ab9a3b69ea224530ef9f";
+
+    var res = await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      },
+    );
+
+    print(jsonDecode(res.body).length);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
